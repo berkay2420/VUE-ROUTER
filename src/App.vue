595e1a1427834913831771ccf221417e3baddba9
Berkay Aydın <99977526+ValyrianAtreides@users.noreply.github.com>
2024-11-19 23:36:42 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script >
+import TheNavigation from '@/components/TheNavigation.vue'
+export default{
+  components: {TheNavigation}
+}
+</script>
+
+<template>
+  <TheNavigation/>
+  
+  <div class="container">
+    <router-view :key="$route.path"></router-view>
+  </div>
+  
+</template>
+
---
+++ src/views/DestinationShow.vue
@@ -0,0 +1,66 @@
+<template>
+  <section v-if="destination">
+    <h1>{{ destination.name }}</h1>
+    <div class="destination-details">
+      <img :src="`/images/${destination.image}`" :alt="destination.name">
+      <p>{{destination.description}}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  computed:{
+    destinationId(){
+      return parseInt(this.$route.params.id)
+    },
+    destination(){
+      return sourceData.destinations.find(
+        (destination) => destination.id === this.destinationId
+      );
+    }
+  }
+  // FETHING DATA FROM https://travel-dummy-api.netlify.app/
+  // data(){
+  //   return {
+  //     destination: null
+  //   }
+  // },
+  // computed: {
+  //   destinationId(){
+  //     return parseInt(this.$route.params.id)
+  //   },
+  //   // destination(){
+  //   //   return sourceData.destinations.find(destination => destination.id === this.destinationId)
+  //   // }
+  // },
+  // methods:{
+  //   async initData(slug){
+  //     try {
+  //       const response = await fetch(`https://travel-dummy-api.netlify.app/${slug}.json`);
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! status: ${response.status}`);
+  //       }
+  //       this.destination = await response.json();
+  //     }catch (error) {
+  //       console.error("Fetch error in initData:", error);
+  //     }
+  //   }
+  // },
+  // async created(){
+  //   try {
+  //     this.initData(this.$route.params.slug)
+  //     // this.$watch(
+  //     //   () => this.$route.params.slug,
+  //     //   async (newSlug) =>{
+  //     //     await this.initData(newSlug);
+  //     //   }
+  //     // )
+  //   }catch (error) {
+  //     console.error("Fetch error:", error);
+  //   }
+  // }
+}
+</script>
